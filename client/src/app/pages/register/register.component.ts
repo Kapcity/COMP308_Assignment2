792f4e51852aaa4f695f8902d10ceef1690e1c2c
Kapilan <Kapster03@gmail.com>
2019-04-05 19:03:22 -0400

    register and login redirects adds to the list

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FlashMessagesService } from "angular2-flash-messages";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
+
+import { User } from "src/app/models/user";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.user = new User();
   }
 
+  onRegisterSubmit(): void {
+    // register the user
+    this.authService.registerUser(this.user).subscribe(data => {
+      if (data.success) {
+        // this.flashMessage.show("You are now registered and can log in", {
+        //   cssClass: "alert-success",
+        //   timeOut: 3000
+        // });
+        console.log("Ey u registered. Lit");
+        this.router.navigate(["/login"]);
+      } else {
+        // this.flashMessage.show("A registration Error occurred", {
+        //   cssClass: "alert-danger",
+        //   timeOut: 3000
+        // });
+        console.log("You a bean");
+        this.router.navigate(["/register"]);
+      }
+    });
+  }
 }
